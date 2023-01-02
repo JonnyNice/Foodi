22b7b2be6f4f86c7ec6e4439cb93c2469e1c9e77
JonnyNice <116230053+JonnyNice@users.noreply.github.com>
2023-01-01 17:55:14 -0800

    Delete practice_app directory

--- practice_app/app/models/Comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ActiveRecord::Base
-    belongs_to :creator
-    belongs_to :recipe
-end
--- practice_app/app/models/Recipe.rb
+++
@@ -1,5 +0,0 @@
-class Recipe < ActiveRecord::Base
-    belongs_to :creator
-    has_many :comments
-    
-end
