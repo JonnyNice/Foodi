d8b24628ac8d60c1268881615ab507f69c99fbcb
William German <william.j.german2@gmail.com>
2023-01-05 15:27:07 -0800

    ???

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,25 +1,65 @@
 import HomeRecipeList from './HomeRecipeList';
 import React from 'react'
 import HomeCreatorList from './HomeCreatorList'
+import homepic from "./pictures/homepic.jpg"
+import "./home.css"
 
 const Home = ({ handleCreatorNameChange }) => {
   return (
-    <div>
-      <div className="App">
-        <header className="App-header">
+    <div className="App">
+      <div className="home__header">
+        <img src={homepic} alt="Header image" />
+        <div className="home__header-text">Foodi</div>
+      </div>
+      <div className="home__content">
+        <div className="middle-section">Featured Recipes
+          <HomeRecipeList />
+        </div>
         <div className='spacer'>--------------</div>
-          <div className = "middle-section">Featured Recipes
-            <HomeRecipeList />
-          </div>
-          <div className='spacer'>--------------</div>
-          <div className = "bottom-section">Featured Creators
-            <HomeCreatorList handleCreatorNameChange={handleCreatorNameChange} />
-          </div>
-          <div className='spacer'>--------------</div>
-        </header>
+        <div className="bottom-section">Featured Creators
+          <HomeCreatorList handleCreatorNameChange={handleCreatorNameChange} />
+        </div>
       </div>
     </div>
   )
 }
 
 export default Home
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Home = ({ handleCreatorNameChange }) => {
+//   return (
+//     <div>
+//       <div className="App">
+//         <header className="App-header">
+//         <div className='spacer'>--------------</div>
+//           <div className = "middle-section">Featured Recipes
+//             <HomeRecipeList />
+//           </div>
+//           <div className='spacer'>--------------</div>
+//           <div className = "bottom-section">Featured Creators
+//             <HomeCreatorList handleCreatorNameChange={handleCreatorNameChange} />
+//           </div>
+//           <div className='spacer'>--------------</div>
+//         </header>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Home
