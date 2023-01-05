cd58ac21af572f350995a56a1c8e5c53315f2497
Ian Holsteen <ianholsteen@gmail.com>
2023-01-05 15:28:09 -0800

    styling

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,6 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_01_03_230126) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "instructions"
+    t.string "image"
+    t.integer "cooktime"
+    t.boolean "spicy"
+    t.boolean "vegan"
+    t.boolean "contains_thc"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "image"
+    t.string "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
